ca94e182290adffd3668240b37dc82009f797062
winston mhango <71964085+winstonmhango23@users.noreply.github.com>
2022-02-22 15:37:09 +0200

    altered style

--- components/Products.js
+++ components/Products.js
@@ -238,7 +238,7 @@ export default function Products(){
               </button>
               <button className="flex justify-center items-center">
                 <svg className="dark:text-white" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M9 6L15 12L9 18" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                  <path d="M9 6L15 12L9 18" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                 </svg>
               </button>
             </div>
